2512bfb417824bc2fead8ad3cb5cf8b4b2e9bedd
Alex <alexsilvestri2000.work@gmail.com>
2026-06-01 18:29:33 +0200

    Fix?

--- js/profile_picture_path_check.js
+++ js/profile_picture_path_check.js
@@ -1,9 +1,13 @@
 window.getProfilePicUrl = function(path) {
     if (!path || path === 'null' || path === '/null') {
-        return '/img/default-avatar.png'
+        return '/img/default-avatar.png';
     }
 
     if (path.startsWith('http')) return path;
+
+    if (path.startsWith('/img/')) {
+        return path; 
+    }
 
     const cleanPath = path.startsWith('/') ? path.substring(1) : path;
     return `${window.APP_CONFIG.BACKEND_URL}/${cleanPath}`;
