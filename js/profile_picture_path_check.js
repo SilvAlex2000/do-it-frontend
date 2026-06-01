02cc9b8b7a7883e45c1f8a7858636ea36728b685
Alex <alexsilvestri2000.work@gmail.com>
2026-06-01 18:53:03 +0200

    Copium

--- js/profile_picture_path_check.js
+++ js/profile_picture_path_check.js
@@ -2,12 +2,12 @@ window.getProfilePicUrl = function(path) {
     if (!path || path === 'null' || path === '/null') {
         return '/img/default-avatar.png';
     }
-
-    if (path.startsWith('http')) return path;
-
-    if (path.includes('/img/default-avatar.png')) {
+	
+	if (path.includes('/img/default-avatar.png')) {
         return '/img/default-avatar.png';
     }
+
+    if (path.startsWith('http')) return path;
 
     const cleanPath = path.startsWith('/') ? path.substring(1) : path;
     return `${window.APP_CONFIG.BACKEND_URL}/${cleanPath}`;
